4206da0da5313d6c23a4a80271306d5359139124
CaroGil <dcgil83@gmail.com>
2026-06-22 13:00:10 -0700

    Se crea la clase Habitacion, con sus respectivos campos y anotaciones.

---
+++ src/main/java/com/devsenior/hotelsystem/model/Habitacion.java
@@ -0,0 +1,43 @@
+package com.devsenior.hotelsystem.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "habitaciones")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Habitacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String numero;
+
+    private String tipo;
+
+    @Column(nullable = false)
+    private Double precioNoche;
+
+    private Boolean disponible;
+
+    @ManyToMany(mappedBy = "habitaciones", cascade = CascadeType.ALL)
+    private Hotel hotel;
+}
