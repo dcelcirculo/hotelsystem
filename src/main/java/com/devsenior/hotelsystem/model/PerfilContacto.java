56da2f07b7503fa6323ddeb087a07e27ae0a68cf
CaroGil <dcgil83@gmail.com>
2026-06-22 14:23:50 -0700

    Se crea la clase PerfilContacto con sus respectivos campos y anotaciones

---
+++ src/main/java/com/devsenior/hotelsystem/model/PerfilContacto.java
@@ -0,0 +1,37 @@
+package com.devsenior.hotelsystem.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "perfil_contacto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PerfilContacto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String telefonoEmergencia;
+    private String direccion;
+    private String pais;
+    private String nacionalidad;
+
+    @OneToOne(mappedBy = "perfilContacto", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Huesped huesped;
+
+}
