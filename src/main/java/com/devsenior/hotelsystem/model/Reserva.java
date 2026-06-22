540b7c5a9504f51cda1d0b2c24f96bd350aafac2
CaroGil <dcgil83@gmail.com>
2026-06-22 14:47:32 -0700

    Se crea la clase Reserva con sus respectivos campos y anotaciones

---
+++ src/main/java/com/devsenior/hotelsystem/model/Reserva.java
@@ -0,0 +1,37 @@
+package com.devsenior.hotelsystem.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fechaIngreso;
+    private LocalDate fechaSalida;
+
+    private String estado;
+    private Double totalCalculado;
+
+    @ManyToOne
+    @JoinColumn(name = "huesped_id")
+    private Huesped huesped;
+
+    @ManyToOne
+    @JoinColumn(name = "habitacion_id")
+    private Habitacion habitacion;
+
+    @ManyToMany
+    @JoinTable(name = "reserva", joinColumns = @JoinColumn(name = "reserva_id"), inverseJoinColumns = @JoinColumn(name = "servicio_id"))
+    private List<Servicio> servicios;
+}
