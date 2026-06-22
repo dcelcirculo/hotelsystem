80c1ea4f4d4ce2e53a837f2052dc18e79dbb0d90
CaroGil <dcgil83@gmail.com>
2026-06-22 13:13:49 -0700

    Corrección de la relación entre hotel - habitacion y las anotaciones de este campo.

--- src/main/java/com/devsenior/hotelsystem/model/Habitacion.java
+++ src/main/java/com/devsenior/hotelsystem/model/Habitacion.java
@@ -1,12 +1,15 @@
 package com.devsenior.hotelsystem.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,6 +38,8 @@ public class Habitacion {
 
     private Boolean disponible;
 
-    @ManyToMany(mappedBy = "habitaciones", cascade = CascadeType.ALL)
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    @JsonBackReference
     private Hotel hotel;
 }
