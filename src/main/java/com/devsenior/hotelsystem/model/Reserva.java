0daeb23a9f6af25b7c96817197d1427d113b4f31
CaroGil <dcgil83@gmail.com>
2026-06-22 14:52:27 -0700

    Se agregan anotaciones faltantes en la clase Reserva.

--- src/main/java/com/devsenior/hotelsystem/model/Reserva.java
+++ src/main/java/com/devsenior/hotelsystem/model/Reserva.java
@@ -3,6 +3,7 @@ package com.devsenior.hotelsystem.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,7 +11,18 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "reservas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reserva {
 
     @Id
