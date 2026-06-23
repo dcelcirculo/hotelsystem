99dfdbd67feb89503fb6807de9bf13a0c4ee6a91
CaroGil <dcgil83@gmail.com>
2026-06-23 13:40:54 -0700

    Se corrige el tipo de dato de las fechas de String a LocalDate y de estado a EstadoReserva en ReservaRepository.

--- src/main/java/com/devsenior/hotelsystem/repository/ReservaRepository.java
+++ src/main/java/com/devsenior/hotelsystem/repository/ReservaRepository.java
@@ -1,23 +1,25 @@
 package com.devsenior.hotelsystem.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
 
+import com.devsenior.hotelsystem.model.EstadoReserva;
 import com.devsenior.hotelsystem.model.Reserva;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
     List<Reserva> findByHuespedId(Long huespedId);
 
-    List<Reserva> findByEstado(String estado);
+    List<Reserva> findByEstado(EstadoReserva estado);
 
     @Query("SELECT r FROM Reserva r WHERE r.fechaIngreso BETWEEN :fechaInicio And :fechaFin")
     List<Reserva> findReservasByFechaIngreso(
-            @Param("fechaInicio") String fechaInicio,
-            @Param("fechaFin") String fechaFin);
+            @Param("fechaInicio") LocalDate fechaInicio,
+            @Param("fechaFin") LocalDate fechaFin);
 
     @Query("SELECT r FROM Reserva r WHERE r.estado = 'CONFIRMADA' OR r.estado = 'PENDIENTE'")
     List<Reserva> findReservasEstadoConfirmadaOPendiente();
